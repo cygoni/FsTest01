f259a7688eb070dc008ff0ae1c0e1d7d58ad2fb4
yg110 <yg931104@gmail.com>
2020-07-21 22:28:24 +0900

    commit 0721

---
+++ FlashscoreTest01/app/src/main/java/com/example/flashscoretest01/Picture_RECActivity.java
@@ -0,0 +1,14 @@
+package com.example.flashscoretest01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Picture_RECActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picture__r_e_c);
+    }
+}
